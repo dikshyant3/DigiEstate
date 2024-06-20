5b0e53a6d374b9923940a332fe190ec6c6ec1018
dikshyant3 <dhunganadikshyant12@gmail.com>
2024-06-20 21:23:28 +0545

    mapped the property to different variables declare a constructor function to initiate the variables

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,27 +2,63 @@
 
 pragma solidity ^0.8.19;
 
-contract Escrow{
+contract RealEstateEscrow {
     address public nftAddress;
-    address public seller;
+    address payable public seller;
     address public inspector;
     address public lender;
 
-    modifier onlyBuyer(uint256 _nftID){
-        require(msg.sender == buyer[_nftID],"Only Buyer can call this method!!!");
+    modifier onlyBuyer(uint256 _nftID) {
+        require(
+            msg.sender == buyer[_nftID],
+            "Only Buyer can call this method!!!"
+        );
         _;
     }
 
-    modifier onlySeller(){
-        require(msg.sender == seller,"Only seller can call this method!!!");
+    modifier onlySeller() {
+        require(msg.sender == seller, "Only seller can call this method!!!");
         _;
     }
 
-    modifier onlyInspector(){
-        require(msg.sender == inspector,"Only inspector can call this method!!!");
+    modifier onlyInspector() {
+        require(
+            msg.sender == inspector,
+            "Only inspector can call this method!!!"
+        );
         _;
     }
 
+    // Mapping to track whether the property is listed for sale
+    mapping(uint256 => bool) public isListed;
+
+    // Mapping to store the purchase price of each property
+    mapping(uint256 => uint256) public purchasePrice;
+
+    // Mapping to store the escrow amount for each property
+    mapping(uint256 => uint256) public escrowAmount;
+
+    // Mapping to associate each property with the buyer's address
     mapping(uint256 => address) public buyer;
-    mapping(uint256 => address) public inspection;
+
+    // Mapping to track the inspection status of each property
+    mapping(uint256 => bool) public inspectionPassed;
+
+    // Mapping to track the approval status from different parties(buyer,seller,inspector,lender) 
+    // for each property
+    mapping(uint256 => mapping(address => bool)) public approval;
+
+    // Constructor function
+
+    constructor(
+        address _nftContract,
+        address payable _propertyOwner,
+        address _propertyInspector,
+        address _propertyLender
+    ){
+        nftAddress = _nftContract;
+        seller = _propertyOwner;
+        inspector= _propertyInspector;
+        lender = _propertyLender;
+    }
 }
