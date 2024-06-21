913e59c01e0d01cc979c2b7042f1b50eaefdafb7
dikshyant3 <dhunganadikshyant12@gmail.com>
2024-06-21 21:44:01 +0545

    function to list the property a function to deposit Escrow amount completed

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,22 +2,30 @@
 
 pragma solidity ^0.8.19;
 
+interface IERC721{
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _id
+    ) external;
+}
+
 contract RealEstateEscrow {
     address public nftAddress;
-    address payable public seller;
+    address payable public owner;
     address public inspector;
     address public lender;
 
-    modifier onlyBuyer(uint256 _nftID) {
+    modifier onlyBuyer(uint256 _nftId) {
         require(
-            msg.sender == buyer[_nftID],
+            msg.sender == buyer[_nftId],
             "Only Buyer can call this method!!!"
         );
         _;
     }
 
-    modifier onlySeller() {
-        require(msg.sender == seller, "Only seller can call this method!!!");
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only seller can call this method!!!");
         _;
     }
 
@@ -30,10 +38,10 @@ contract RealEstateEscrow {
     }
 
     // Mapping to track whether the property is listed for sale
-    mapping(uint256 => bool) public isListed;
+    mapping(uint256 => bool) public isPropertyListed;
 
     // Mapping to store the purchase price of each property
-    mapping(uint256 => uint256) public purchasePrice;
+    mapping(uint256 => uint256) public propertyPrice;
 
     // Mapping to store the escrow amount for each property
     mapping(uint256 => uint256) public escrowAmount;
@@ -57,8 +65,34 @@ contract RealEstateEscrow {
         address _propertyLender
     ){
         nftAddress = _nftContract;
-        seller = _propertyOwner;
+        owner = _propertyOwner;
         inspector= _propertyInspector;
         lender = _propertyLender;
+    }
+
+    //  Function to list a property for sale
+
+    function listProperty(
+        uint256 _nftId,
+        address _buyer,
+        uint256 _propertyPrice,
+        uint256 _escrowAmount
+    ) public payable onlyOwner{
+
+        //  Transfer the NFT from owner to this contract
+        IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftId);
+
+        // Update the property status
+
+        isPropertyListed[_nftId] = true;
+        propertyPrice[_nftId] = _propertyPrice;
+        escrowAmount[_nftId] = _escrowAmount;
+        buyer[_nftId] = _buyer;
+    }
+
+    // Function to deposit escrow funds
+
+    function depositEscrow(uint256 _nftId) public payable onlyBuyer(_nftId){
+        require(msg.value >= escrowAmount[_nftId] , "Insufficient funds for the escrow");
     }
 }
