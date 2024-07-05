69f7f6d50a398cea2593ef57a14bbb9d08fc19ec
dikshyant3 <dhunganadikshyant12@gmail.com>
2024-07-05 22:07:19 +0545

    etherjs connection with the wla

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,64 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { ethers } from 'ethers'
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import Search from './components/Search'
+import RealEstate from './artifacts/contracts/RealEstate.sol/RealEstate.json'
+import Escrow from './artifacts/contracts/Escrow.sol/RealEstate.json'
+import config from './config.json' // address json from metamask
 
 
 function App() {
+  const [provider, setProvider] = useState(null)
+  const [escrow, setEscrow] = useState(null)
+  const [account, setAccount] = useState(null)
+  const [homes, setHomes] = useState([])
+  const [home, setHome] = useState({})
+  const [toggle, setToggle] = useState(false)
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+    
+    const network = await provider.getNetwork()
+    
+    const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider)
+    const totalSupply = await realEstate.totalSupply()
+    const homes = []
+    
+    for (var i = 1; i <= totalSupply; i++) {
+      const uri = await realEstate.tokenURI(i)
+      const response = await fetch(uri)
+      const metadata = await response.json()
+      homes.push(metadata)
+    }
+    setHomes(homes)
+    
+    const escrow = new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider)
+    setEscrow(escrow)
+    
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account);
+    })
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
+  const togglePop = (home) => {
+    setHome(home)
+    setToggle(!toggle)
+  }
+
 
   return (
     <div className='max-w-screen h-screen'>
       <Navbar />
-      <Search/>
-      <Home/>
+      <Search />
+      <Home />
     </div>
   )
 }
