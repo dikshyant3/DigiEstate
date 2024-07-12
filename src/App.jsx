ad2940af659251374d9674559e2fb1f286ae9f6a
Subash Jirel <subashjrl@gmail.com>
2024-07-12 22:19:53 +0545

    connect the react app to metamask

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,68 @@
-import { useState, useEffect } from 'react'
-import { ethers } from 'ethers'
-import Navbar from "./components/Navbar"
-import Home from "./components/Home"
-import Search from './components/Search'
-import RealEstate from './abis/RealEstate.json'
-import RealEstateEscrow from './abis/RealEstateEscrow.json'
-// import config from './config.json'  address json from metamask
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
 
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Search from './components/Search';
+
+import RealEstate from './abis/RealEstate.json';
+import RealEstateEscrow from './abis/RealEstateEscrow.json';
+import config from './config.json'; //address json from metamask
 
 function App() {
-  const [provider, setProvider] = useState(null)
-  const [escrow, setEscrow] = useState(null)
-  const [account, setAccount] = useState(null)
-  const [homes, setHomes] = useState([])
-  const [home, setHome] = useState({})
-  const [toggle, setToggle] = useState(false)
+  const [provider, setProvider] = useState(null);
+  const [escrow, setEscrow] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [homes, setHomes] = useState([]);
+  const [home, setHome] = useState({});
+  const [toggle, setToggle] = useState(false);
 
-  const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(provider)
-    
-    const network = await provider.getNetwork()
-    
-    // const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider)
-    const totalSupply = await realEstate.totalSupply()
-    const homes = []
-    
-    for (var i = 1; i <= totalSupply; i++) {
-      const uri = await realEstate.tokenURI(i)
-      const response = await fetch(uri)
-      const metadata = await response.json()
-      homes.push(metadata)
-    }
-    setHomes(homes)
-    
-    // const escrow = new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider)
-    setEscrow(escrow)
-    
-    window.ethereum.on('accountsChanged', async () => {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      const account = ethers.utils.getAddress(accounts[0])
-      setAccount(account);
-    })
-  }
+  // const loadBlockchainData = async () => {
+  //   const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //   setProvider(provider);
 
-  useEffect(() => {
-    loadBlockchainData()
-  }, [])
+  //   const network = await provider.getNetwork();
+
+  //   // const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider)
+  //   const totalSupply = await realEstate.totalSupply();
+  //   const homes = [];
+
+  //   for (var i = 1; i <= totalSupply; i++) {
+  //     const uri = await realEstate.tokenURI(i);
+  //     const response = await fetch(uri);
+  //     const metadata = await response.json();
+  //     homes.push(metadata);
+  //   }
+  //   setHomes(homes);
+
+  //   // const escrow = new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider)
+  //   setEscrow(escrow);
+
+  //   window.ethereum.on('accountsChanged', async () => {
+  //     const accounts = await window.ethereum.request({
+  //       method: 'eth_requestAccounts',
+  //     });
+  //     const account = ethers.utils.getAddress(accounts[0]);
+  //     setAccount(account);
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   loadBlockchainData();
+  // }, []);
 
   const togglePop = (home) => {
-    setHome(home)
-    setToggle(!toggle)
-  }
-
+    setHome(home);
+    setToggle(!toggle);
+  };
 
   return (
-    <div className='max-w-screen h-screen'>
+    <div className="max-w-screen h-screen">
       <Navbar />
       <Search />
       <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,86 @@
+import { ethers } from 'ethers';
+import { useState } from 'react';
+
 const Navbar = () => {
-    return (
-        <nav className="w-full h-20 flex flex-col items-center justify-between border-2 border-red-500 bg-white text-black z-10">
-            <div className="w-full flex justify-between items-center h-16 bg-white text-xl px-4 md:text-2xl">
-                <div className="flex space-x-4 gap-2 border-2 border-red-800">
-                    <a className="border-2 border-blue-700 px-2" href="#">Buy</a>
-                    <a className="border-2 border-blue-700 px-2" href="#">Sell</a>
-                    <a className="border-2 border-blue-700 px-2" href="#">Rent</a>
-                </div>
-                <div className="text-xl font-semibold lg:text-4xl">
-                    <h1>DigiEstate</h1>
-                </div>
-                <div>
-                    <button type="button" className="bg-blue-700 text-white text-lg md:text-2xl px-4 py-2 border-none rounded-md ring-0 hover:bg-blue-500">Connect</button>
-                </div>
-            </div>
-        </nav>
-    );
-}
+  //newWay
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [userBalance, setUserBalance] = useState(null);
+  const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+  const connectWalletHandler = () => {
+    if (window.ethereum && window.ethereum.isMetaMask) {
+      console.log('MetaMask Here!');
+
+      window.ethereum
+        .request({ method: 'eth_requestAccounts' })
+        .then((result) => {
+          accountChangedHandler(result[0]);
+          setConnButtonText('Wallet Connected');
+          getAccountBalance(result[0]);
+        })
+        .catch((error) => {
+          setErrorMessage(error.message);
+        });
+    } else {
+      console.log('Need to install MetaMask');
+      setErrorMessage('Please install MetaMask browser extension to interact');
+    }
+  };
+  // update account, will cause component re-render
+  const accountChangedHandler = (newAccount) => {
+    setDefaultAccount(newAccount);
+    getAccountBalance(newAccount.toString());
+  };
+
+  const getAccountBalance = (account) => {
+    window.ethereum
+      .request({ method: 'eth_getBalance', params: [account, 'latest'] })
+      .then((balance) => {
+        setUserBalance(ethers.utils.formatEther(balance));
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  };
+
+  const chainChangedHandler = () => {
+    // reload the page to avoid any errors with chain change mid use of application
+    window.location.reload();
+  };
+
+  // listen for account changes
+  window.ethereum.on('accountsChanged', accountChangedHandler);
+
+  window.ethereum.on('chainChanged', chainChangedHandler);
+  return (
+    <nav className="w-full h-20 flex flex-col items-center justify-between border-2 border-red-500 bg-white text-black z-10">
+      <div className="w-full flex justify-between items-center h-16 bg-white text-xl px-4 md:text-2xl">
+        <div className="flex space-x-4 gap-2 border-2 border-green-800">
+          <a className="border-2 border-blue-700 px-2" href="#">
+            Buy
+          </a>
+          <a className="border-2 border-blue-700 px-2" href="#">
+            Sell
+          </a>
+          <a className="border-2 border-blue-700 px-2" href="#">
+            Rent
+          </a>
+        </div>
+        <div className="text-xl font-semibold lg:text-4xl">
+          <h1>DigiEstate</h1>
+        </div>
+        <div>
+          <button
+            onClick={connectWalletHandler}
+            type="button"
+            className="bg-blue-700 text-white text-lg md:text-2xl px-4 py-2 border-none rounded-md ring-0 hover:bg-blue-500"
+          >
+            {connButtonText}
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
