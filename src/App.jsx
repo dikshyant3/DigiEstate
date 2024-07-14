b31bc98cfa957541d84fe57aef622e1f0f0bb62b
Subash Jirel <subashjrl@gmail.com>
2024-07-14 13:11:42 +0545

    the homes are fetched and displayed

--- src/App.jsx
+++ src/App.jsx
@@ -17,36 +17,6 @@ function App() {
   const [home, setHome] = useState({});
   const [toggle, setToggle] = useState(false);
 
-  // const loadBlockchainData = async () => {
-  //   const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //   setProvider(provider);
-
-  //   const network = await provider.getNetwork();
-
-  //   // const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider)
-  //   const totalSupply = await realEstate.totalSupply();
-  //   const homes = [];
-
-  //   for (var i = 1; i <= totalSupply; i++) {
-  //     const uri = await realEstate.tokenURI(i);
-  //     const response = await fetch(uri);
-  //     const metadata = await response.json();
-  //     homes.push(metadata);
-  //   }
-  //   setHomes(homes);
-
-  //   // const escrow = new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider)
-  //   setEscrow(escrow);
-
-  //   window.ethereum.on('accountsChanged', async () => {
-  //     const accounts = await window.ethereum.request({
-  //       method: 'eth_requestAccounts',
-  //     });
-  //     const account = ethers.utils.getAddress(accounts[0]);
-  //     setAccount(account);
-  //   });
-  // };
-
   const loadBlockchainData = async () => {
     // const provider = new ethers.providers.Web3Provider(window.ethereum); // this is v5
 
@@ -61,6 +31,7 @@ function App() {
       provider
     );
     const totalSupply = await realEstate.totalSupply();
+    const homes = [];
 
     //Check this cannot fetch the homes
     for (var i = 1; i < totalSupply; i++) {
@@ -70,7 +41,7 @@ function App() {
 
         // Logging the response to inspect it
         const text = await response.text();
-        console.log(`Response from ${uri}:`, text);
+        // console.log(`Response from ${uri}:`, text);
 
         const metadata = JSON.parse(text); // Parsing the response text as JSON
         homes.push(metadata);
@@ -83,6 +54,7 @@ function App() {
     }
 
     setHomes(homes);
+    console.log('This is the homes');
     console.log(homes);
     const escrow = new ethers.Contract(
       config[network.chainId].escrow.address,
@@ -104,6 +76,39 @@ function App() {
     <div className="max-w-screen h-screen">
       <Navbar />
       <Search />
+      <div className="homesSection border-2 border-red-600">
+        <h3 className="text-3xl font-bold text-gray-900 text-center mt-4 mb-2 ">
+          Homes for you
+        </h3>
+        <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 justify-center border-2 px-5 py-3  border-green-500 ">
+          {homes.map((home) => (
+            <>
+              <div
+                className="w-[300px] rounded-md border cursor-pointer"
+                key={home.id}
+              >
+                <img
+                  src={home.image}
+                  alt="Laptop"
+                  className="h-[200px] w-full rounded-md object-cover"
+                />
+                <div className="p-4">
+                  <h1 className="text-lg font-semibold">{home.name}</h1>
+                  <p className="mt-3 text-sm text-gray-600">
+                    {home.description}
+                  </p>
+                  <button
+                    type="button"
+                    className="mt-4  rounded-sm bg-black px-4 py-2 text-[14px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                  >
+                    {home.attributes[0].value} ETH
+                  </button>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+      </div>
       <Home />
     </div>
   );
